c9c7e4811d65e17c4241fd20119d8974d8b1c11d
unknown <you@example.com>
2021-10-25 22:41:39 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import About from './views/About';
+import Home from './views/Home';
+import { Route } from 'react-router-dom'
+import Racers from './views/Racers';
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  handleClick = (step) => {
+    let newCount = this.state.count + step
+    this.setState({
+        count: newCount
+    })
+  }
+
+  render() {
+    return (
+      <>
+      <Navbar />
+      <div className='container'>
+        <Route exact path='/'>
+          <Home count={this.state.count} handleClick={this.handleClick} name={this.props.name}/>
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/racers'>
+          <Racers />
+        </Route>
+
+      </div>
+      </>
+    )
+  }
+}
---
+++ src/views/Users.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import UserInfo from '../components/RacerDetail';
+
+export default class Users extends Component {
+    constructor(props){
+        super(props);
+        console.log('User component constructed')
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount(){
+        console.log('User component mounted')
+        fetch('https://kekambas-bs.herokuapp.com/kekambas')
+        .then(res => res.json())
+        .then(data => this.setSteate({
+            users: data.id[19].first_name
+            users: data.id[19].last_name
+        }))
+    }
+    render() {
+        console.log('User name renadered')
+        console.log(this.state)
+        return (
+            <div>
+                adslfhaohfislfhkashkvnds
+                {this,state.users.map((u,i) => <UserInfo key={i} user = {u}  />)}
+            </div>
+        )
+    }
+}
